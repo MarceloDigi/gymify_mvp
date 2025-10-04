966f0a517e9773e1f3d50fc33ea57744eac1a95d
MarceloCruz <marcelocruz_1999@hotmail.com>
2025-10-04 17:08:40 +0200

    [feat] dump script

---
+++ services/dump_data.py
@@ -0,0 +1,11 @@
+import database.db_connector as connector
+import pandas as pd
+
+def dump_into_sql(df: pd.DataFrame, table_name: str, oltp_db: bool = True) -> None:
+    try:
+        engine = connector.get_engine(oltp_db=oltp_db)
+        with engine.begin() as connection:
+            df.to_sql(table_name, con=connection, if_exists='append', index=False)
+            print(f'Añadido exitosamente a la tabla: {table_name} - {len(df)} filas')
+    except Exception as e:
+        print(f'❌ Error al intentar añadir a la tabla {table_name}: {e}')
